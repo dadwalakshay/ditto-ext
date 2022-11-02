7625bcfec9b3eef643b644aaf155c8976dca2bc3
dadwalakshay <akshayd00@outlook.com>
2022-11-03 03:13:12 +0530

    Initializing ditto-ext extension for chromium based browsers.

---
+++ ditto.js
@@ -0,0 +1,20 @@
+function copyToRemote (userInput) {
+  navigator.clipboard.writeText(userInput).then(
+    () => {
+      // Nothing needs to be done here.
+    },
+    (e) => {
+  	  alert(`Ditto failed {e}`);
+    }
+  );
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let pasteButton = document.getElementById("ditto-paste-button");
+
+  pasteButton.addEventListener("click", function() {
+    let userInput = document.getElementById("ditto-user-input").value;
+
+	  copyToRemote(userInput);
+  }, false);
+}, false);
